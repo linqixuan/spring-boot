4df18543b187d1a5db0c379090017ded47f9c3bb
linqixuan <66407745@qq.com>
2022-06-09 09:47:11 +0800

    新建测试模块

---
+++ spring-boot-tests/spring-boot-smoke-tests/spring-boot-smoke-test-web/src/main/java/smoketest/web/SimpleWebApplication.java
@@ -0,0 +1,20 @@
+package smoketest.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author linqx
+ * @version 1.0
+ * @classname SimpleWebApplication
+ * @description TODO
+ * @date 2022/6/9 9:28
+ */
+@SpringBootApplication
+public class SimpleWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWebApplication.class, args);
+	}
+
+}
